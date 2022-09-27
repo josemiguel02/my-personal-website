403c1bea89b6fd1b2b51a80ca48e84190436f09a
josemiguel02 <josemidev24@gmail.com>
2022-09-27 12:44:00 -0500

    updated footer styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react'
-import { Flex, Box, Text, useColorModeValue } from '@chakra-ui/react'
-import { CustomImage } from './CustomImage'
+import { Flex, Text, useColorModeValue } from '@chakra-ui/react'
 import { LinkItem } from './LinkItem'
 
 export const Footer = () => {
@@ -11,49 +10,30 @@ export const Footer = () => {
   }, [])
 
   return (
-    <Box as='footer' w='full' h='220px' pos='relative'>
-      <Box
-        as='figure'
-        w='full'
-        h='full'
-        pos='absolute'
-        inset={0}
-        zIndex={-1}
-        filter='blur(40px)'
-        opacity={useColorModeValue(0.4, 0.9)}
-      >
-        <CustomImage
-          src='/static/images/waves-gradient.svg'
-          alt='waves bg'
-          layout='fill'
-          objectFit='cover'
-        />
-      </Box>
-
-      <Flex
-        justify='flex-end'
-        align='center'
-        direction='column'
-        h='full'
-        p={4}
-        color={useColorModeValue('gray.700', 'darkGray')}
-      >
-        <Text fontSize='xs' color='inherit'>
-          Designed & Coded with 🖤 by{' '}
-          <LinkItem
-            href='https://github.com/josemiguel02'
-            transition='color .2s ease-in-out'
-            _hover={{
-              color: useColorModeValue('gray.800', '#fff')
-            }}
-          >
-            @josemidev24
-          </LinkItem>
-        </Text>
-        <Text fontSize='xs' color='inherit'>
-          &copy; {currentYear} José Domínguez P.
-        </Text>
-      </Flex>
-    </Box>
+    <Flex
+      p={3}
+      mt='10rem'
+      as='footer'
+      h='full'
+      align='center'
+      direction='column'
+      color={useColorModeValue('gray.700', 'darkGray')}
+    >
+      <Text fontSize='xs' color='inherit'>
+        Designed & Coded with 🖤 by{' '}
+        <LinkItem
+          href='https://github.com/josemiguel02'
+          transition='color .2s ease-in-out'
+          _hover={{
+            color: useColorModeValue('gray.800', '#fff')
+          }}
+        >
+          @josemidev24
+        </LinkItem>
+      </Text>
+      <Text fontSize='xs' color='inherit'>
+        &copy; {currentYear} José Domínguez P.
+      </Text>
+    </Flex>
   )
 }
