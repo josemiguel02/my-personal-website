7d470a2e9bb1f37968689367d0855c7134886258
josemiguel02 <josemidev24@gmail.com>
2022-09-27 12:39:24 -0500

    updated types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,6 +2,25 @@
 import { FC, PropsWithChildren, Dispatch, SetStateAction } from 'react'
 import { IconType } from 'react-icons'
 
+type Tag =
+  | 'HTML'
+  | 'CSS'
+  | 'SCSS'
+  | 'JavaScript'
+  | 'TypeScript'
+  | 'React.JS'
+  | 'React Native'
+  | 'Vue.JS'
+  | 'Vuex'
+  | 'Node.JS'
+  | 'PHP'
+  | 'Bootstrap'
+  | 'TailwindCSS'
+  | 'Expo'
+  | 'JQuery'
+  | 'MySQL'
+  | 'Firebase'
+
 declare global {
   type FCC<P = {}> = FC<PropsWithChildren<P>>
 
@@ -20,6 +39,16 @@ declare global {
     name: string
     skills: Array<string>
     iconsList: Array<IIconList>
+  }
+
+  interface IProject {
+    id: string
+    title: string
+    image: string
+    description: string
+    previewLink: string
+    repoLink: string
+    tags: Array<Tag>
   }
 }
 
