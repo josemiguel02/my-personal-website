bb10d5e9da5cd99f27a1d9e0fed5cf4e9d85bb6f
josemiguel02 <josemidev24@gmail.com>
2022-09-27 12:41:07 -0500

    updated fonts

--- src/components/Fonts.tsx
+++ src/components/Fonts.tsx
@@ -4,7 +4,7 @@ export const Fonts = () => {
   return (
     <Global
       styles={`
-        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap');
       `}
     />
   )
